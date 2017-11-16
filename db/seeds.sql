8fd7b4d77bfc7b6761efbbdde5d1415010901428
jhartatx <jhartatx@gmail.com>
2017-11-16 01:52:20 -0600

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tequila Burger', true);
---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(name, cb) {
+    orm.create("burgers", "burger_name", name, cb);
+  },
+  update: function(id, cb) {
+    var condition = id;
+
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
